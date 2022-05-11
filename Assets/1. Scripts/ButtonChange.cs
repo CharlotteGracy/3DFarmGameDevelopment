8144200e8b7460976068aa5e06382c5f57226998
CharlotteGracy <gracenephele@naver.com>
2022-05-11 16:25:56 +0900

    시작 메인 화면 구현

---
+++ Assets/1. Scripts/ButtonChange.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class ButtonChange : MonoBehaviour
+{
+
+    public GameObject SettingsUI;
+    public GameObject ExitUI;
+    public Button noButton, yesButton, backButton;
+  
+
+    public void GameScenePlay(){
+        SceneManager.LoadScene("GameScene");
+
+    }
+    public void SettingsUIOn(){
+        SettingsUI.SetActive(true);
+        backButton.onClick.AddListener(()=>{SettingsUI.SetActive(false);});
+
+    }
+  
+ 
+    public void ExitUIOn(){
+        
+        ExitUI.SetActive(true);
+    
+        noButton.onClick.AddListener(()=>{ ExitUI.SetActive(false);});
+        yesButton.onClick.AddListener(Exit);
+    }
+ 
+   
+    public void Exit(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+
+    }
+
+    
+    
+}
---
+++ Assets/1. Scripts/PlayerMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    private Rigidbody rigid;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+	private	CharacterController	characterController;
+
+    private Vector3 moveVec;
+    private float moveH;
+    private float moveV;
+
+    
+
+
+private void Update()
+{
+    Move();
+}
+
+    private void Move(){
+        moveH = Input.GetAxis("Horizontal");
+        moveV = Input.GetAxis("Vertical");
+
+        moveVec = transform.right*moveH + transform.forward*moveV;
+        characterController.Move(moveVec * moveSpeed * Time.deltaTime);
+
+    }
+    
+    
+}
