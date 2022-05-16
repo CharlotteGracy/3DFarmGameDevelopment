1def1703ca5fed3d048fdfa2ffb7ceba19dea0b8
CharlotteGracy <gracenephele@naver.com>
2022-05-16 21:51:17 +0900

    배경 오브젝트에 Mesh Collider 삽입 및 Tool 위치 고정

--- Assets/1. Scripts/GameSceneButtonChange.cs
+++ Assets/1. Scripts/GameSceneButtonChange.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameSceneButtonChange : MonoBehaviour
 {
     public GameObject KitchenUI;
+    public GameObject ToolUI;
+    public Button kitchenCloseButton, toolCloseButton;
 
 
     public void GoToBarn(){
+        Debug.Log("Barn!");
+        SceneManager.LoadScene("BarnScene");
 
     }
 
     public void KitchenUIOn(){
-        
+        Debug.Log("Kitchen!");
+        KitchenUI.SetActive(true);
+        kitchenCloseButton.onClick.AddListener(()=>{KitchenUI.SetActive(false);});
+
+    }
+
+    public void ToolUIOn(){
+        ToolUI.SetActive(true);
+        toolCloseButton.onClick.AddListener(()=>{ToolUI.SetActive(false);});
     }
     
 }
